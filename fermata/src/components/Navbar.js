7fca917367c2e1e7bc3ed1133eadda07015554fc
Justi Sasser <justi.sasser@gmail.com>
2025-04-05 16:34:52 +0200

    1.1.4: add home to nav bar

--- fermata/src/components/Navbar.js
+++ fermata/src/components/Navbar.js
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="bg-green-900 text-white p-4 shadow-lg">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
-
-        {/* Navigation Links */}
-        <div className="flex space-x-6 items-center">
+      <div className="max-w-7xl mx-auto">
+        {/* Main Navigation Links */}
+        <div className="flex justify-between items-center">
           <Link to="/" className="hover:text-green-300 transition">Home</Link>
           <Link to="/offerings" className="hover:text-green-300 transition">Offerings</Link>
+        </div>
+
+        {/* Sub-navigation for Offerings */}
+        <div className="flex justify-center space-x-6 mt-2">
           <Link to="/offerings/wellness" className="hover:text-green-300 transition">Wellness</Link>
           <Link to="/offerings/faith" className="hover:text-green-300 transition">Faith</Link>
           <Link to="/offerings/coaching" className="hover:text-green-300 transition">Coaching</Link>
