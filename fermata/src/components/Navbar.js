2f4d444af85af38e85106289097a309c0c0a74cd
Justi Sasser <justi.sasser@gmail.com>
2025-04-05 16:19:37 +0200

    1.1.4: add home to nav bar

--- fermata/src/components/Navbar.js
+++ fermata/src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Branding */}
         <h1 className="text-2xl font-bold">
-          <Link to="/">Fermata</Link>
+          <Link to="/">Fermata Lifestyle Reset Centre</Link>
         </h1>
 
         {/* Navigation Links */}
