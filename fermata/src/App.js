1a32a70f86fa9c046c16e81a134d40ac13a366ff
Justi Sasser <justi.sasser@gmail.com>
2025-04-05 16:07:42 +0200

    1.1.4: add pages

--- fermata/src/App.js
+++ fermata/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import FermataHome from './pages/FermataHome';
 import Coaching from './pages/Offerings/Coaching';
@@ -13,15 +13,15 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={FermataHome} />
-        <Route path="/offerings/coaching" component={Coaching} />
-        <Route path="/offerings/detox" component={Detox} />
-        <Route path="/offerings/faith" component={Faith} />
-        <Route path="/offerings/retreat" component={Retreat} />
-        <Route path="/offerings/wellness" component={Wellness} />
-        <Route path="/offerings/workshop" component={Workshop} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<FermataHome />} />
+        <Route path="/offerings/coaching" element={<Coaching />} />
+        <Route path="/offerings/detox" element={<Detox />} />
+        <Route path="/offerings/faith" element={<Faith />} />
+        <Route path="/offerings/retreat" element={<Retreat />} />
+        <Route path="/offerings/wellness" element={<Wellness />} />
+        <Route path="/offerings/workshop" element={<Workshop />} />
+      </Routes>
     </Router>
   );
 }
