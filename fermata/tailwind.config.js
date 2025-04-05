5dcd4b500b746de659fe082d26c34d150015ee93
Justi Sasser <justi.sasser@gmail.com>
2025-04-05 12:59:29 +0200

    Final structure for clean deploy

--- fermata/tailwind.config.js
+++ fermata/tailwind.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -6,4 +5,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+}
